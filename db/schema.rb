facc18e56ff5aaf93286dfd7eeb0241519977df1
Julian Pittleman <jnpn87@gmail.com>
2013-10-19 12:10:15 -0400

    userpage join

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019152425) do
+ActiveRecord::Schema.define(version: 20131019160737) do
 
   create_table "pages", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "src"
     t.float    "amount"
+  end
+
+  create_table "pages_users", id: false, force: true do |t|
+    t.integer "user_id", null: false
+    t.integer "page_id", null: false
   end
 
   create_table "users", force: true do |t|
