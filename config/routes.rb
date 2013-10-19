0522ca86ae96fc3bceae39b4429643ca7355f091
Jorge Olivero <jorgeaolivero@gmail.com>
2013-10-19 11:37:44 -0400

    Theme implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Formation::Application.routes.draw do
   resources :pages
 
+  root :to => "pages#home"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -9,7 +11,6 @@ Formation::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
