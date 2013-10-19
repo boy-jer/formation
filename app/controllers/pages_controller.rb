7a7eb979544cd66e7855d180d4fb95626bf7cff3
Kyle Dorman <ked65@cornell.edu>
2013-10-19 12:33:43 -0400

    stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,11 @@ class PagesController < ApplicationController
     @page = Page.new
   end
 
-  
+  def home_submit
+    User.new(first_name: params[:first_name])
+  end
+
+
   def show
     # signupinfo = SignUpInfo.find_by_name(:name)
     # signup = Page.new(amount: 25.0 , src: "https://docs.google.com/forms/d/1JoqfHA2YYBXus0TEONpcnaUf9UMQEn-y2UZJ73vNuWw/viewform?embedded=true")
@@ -16,8 +20,12 @@ class PagesController < ApplicationController
   end
 
   def submit
-    signupinfo = SignUpInfo.find_by_name(:name)
-    stripe_code(signupinfo)
+    binding.pry
+    @page.stripe_code(params[:stripeToken])
+  end
+
+  def thanks
+
   end
 
   private
@@ -28,6 +36,6 @@ class PagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def page_params
-      params[:page]
+      params[:stripeToken]
     end
 end
